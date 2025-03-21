6230b759efb9cc1d30dec8825a7826b40c003b27
Nahuel-xpag <agustin.nahuu@gmail.com>
2025-03-21 06:35:55 +0000

    progress: added ejs logic to message board

--- app.js
+++ app.js
@@ -7,12 +7,31 @@ const LocalStrategy = require('passport-local');
 const bcrypt = require('bcryptjs');
 require('dotenv').config();
 
-const pool = new Pool(
+/*const pool = new Pool(
 process.env.DBOPTIONS
-)
+)*/
 
 const app = express();
 
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(express.urlencoded({extended: true}));
+app.get("/", (req, res) => res.render("index", {
+messages: [
+    {
+    user: "username",
+    body: "message body sample"
+    },
+    {
+    user: "other user",
+    body: "other body message sample" 
+    }
+]
+}
+));
 
+
+app.listen(3000, () => console.log("App listening on port 3000!"))
